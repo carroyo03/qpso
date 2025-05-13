0ac63cea385d310950e21429d1e6d44e942ce95d
carroyo03 <cgarroyolorenzo@gmail.com>
2025-05-14 00:37:14 +0200

    Remove unused data and analysis scripts.
    Deleted obsolete files: `__init__.py`, `preprocessing.py`, and parts of `data.ipynb`. These were no longer relevant to the current project workflow or analysis pipeline.

--- data/preprocessing.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd #type: ignore
-import torch #type: ignore
-from sklearn.model_selection import train_test_split #type: ignore
-from sklearn.preprocessing import StandardScaler #type: ignore
-from ucimlrepo import fetch_ucirepo #type: ignore
-
-website_phishing = fetch_ucirepo(id=379)
-X = website_phishing.data.features
-y = website_phishing.data.targets
